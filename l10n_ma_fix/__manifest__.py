5ed1581570f241d57975ad6f1628b2419f1def19
BADEP <k.hazam@badep.ma>
2019-06-15 13:14:39 +0100

    WIP: l10n_ma_fix

---
+++ l10n_ma_fix/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_ma_fix",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['l10n_ma'],
+
+    'data': [
+        'data/account_tax_data.xml',
+        'data/l10n_ma_chart_data.xml',
+        'data/res_country_data.xml',
+        'views/report_invoice.xml',
+    ],
+
+}
