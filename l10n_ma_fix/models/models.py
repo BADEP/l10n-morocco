1fad43d83f9414c0b394ec22709e1f9c7412cad8
BADEP <k.hazam@badep.ma>
2019-06-15 19:05:39 +0100

    add: cash_basis vat

--- l10n_ma_fix/models/models.py
+++ l10n_ma_fix/models/models.py
@@ -21,7 +21,7 @@ class AccountAccount(models.Model):
     _inherit = 'account.account'
 
     @api.model
-    #Mise à jour des sociétés existantes. Les nouvelles
+    #Mise à jour des sociétés existantes. Les nouvelles sociétés auront le paramétrge automatique
     def init_migration(self):
         for company in self.env['res.company'].search([('chart_template_id', '=', self.env.ref('l10n_ma.l10n_kzc_temp_chart').id)]):
             self.env['ir.property'].search([('name', '=', 'property_account_receivable_id'), ('company_id', '=', company.id)]).write({
@@ -38,6 +38,26 @@ class AccountAccount(models.Model):
             #TODO: Ne pas créer de position fiscale à la mise à jour du module ou si la position fiscale "étranger" est déjà dans le système
             self.env['account.fiscal.position'].create({
                 'name': 'Etranger',
+                'company_id': company.id,
                 'tax_ids': [(0, 0, {'tax_src_id': self.env.ref('l10n_ma.' + str(company.id) +'_tva_vt20').id, 'tax_dest_id': self.env.ref('l10n_ma.' + str(company.id) +'_tva_exo').id}), (0, 0, {'tax_src_id': self.env.ref('l10n_ma.' + str(company.id) +'_tva_ac20').id, 'tax_dest_id': self.env.ref('l10n_ma.' + str(company.id) +'_tva_exo').id})],
                 'account_ids': [(0, 0, {'account_src_id': self.env.ref('l10n_ma.' + str(company.id) +'_pcg_7111').id, 'account_dest_id': self.env.ref('l10n_ma.' + str(company.id) +'_pcg_7113').id})]
+            })
+            self.env['account.journal'].create({
+                'name': 'Chèques',
+                'type': 'bank',
+                'code': 'CHQ',
+                'company_id': company.id,
+                'default_debit_account_id': self.env.ref('l10n_ma.' + str(company.id) +'_pcg_51111').id,
+                'default_credit_account_id': self.env.ref('l10n_ma.' + str(company.id) +'_pcg_51111').id,
+                'post_at_bank_rec': True
+            })
+            self.env['account.tax'].search([('company_id', '=', company.id), ('type_tax_use', '=', 'sale')]).write({
+                'tax_exigibility': 'on_payment',
+                'cash_basis_account_id': self.env.ref('l10n_ma.' + str(company.id) +'_pcg_4456').id,
+                'cash_basis_base_account_id': self.env.ref('l10n_ma.' + str(company.id) +'_pcg_7111').id
+            })
+            self.env['account.tax'].search([('company_id', '=', company.id), ('type_tax_use', '=', 'purchase')]).write({
+                'tax_exigibility': 'on_payment',
+                'cash_basis_account_id': self.env.ref('l10n_ma.' + str(company.id) +'_pcg_3456').id,
+                'cash_basis_base_account_id': self.env.ref('l10n_ma.' + str(company.id) +'_pcg_6111').id
             })
